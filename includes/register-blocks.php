fddbf8e1ad6efe4b5facc2fbb12029d293321b3e
VictorScoolExper <victorscool477@outlook.com>
2023-02-23 17:31:11 -0600

    break time

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -2,12 +2,19 @@
 
 function up_register_blocks(){
     $blocks = [
-        [ 'name' => 'fancy-header' ]
+        [ 'name' => 'fancy-header' ],
+        [ 'name' => 'search-form', 'options' => [
+            'render_callback' => 'up_search_form_render_cb'
+        ]],
+        [ 'name' => 'page-header', 'options' => [
+            'render_callback' => 'up_page_header_render_cb'
+        ]]
     ];
 
     foreach($blocks as $block){
         register_block_type( 
-            UP_PLUGIN_DIR . 'build/blocks/' . $block['name'] // we can have access to the file before it 
+            UP_PLUGIN_DIR . 'build/blocks/' . $block['name'], // we can have access to the file before it 
+            isset($block['options']) ? $block['options'] : []
         );
     } 
 }
--- udemy-plus.php
+++ udemy-plus.php
@@ -25,7 +25,23 @@
 define('UP_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 
  // Includes
- include(UP_PLUGIN_DIR . 'includes/register-blocks.php');
+ // glob or global will be used to get all files
+ $rootFiles = glob(UP_PLUGIN_DIR . 'includes/*.php');
+ $subdirectoryFiles = glob(UP_PLUGIN_DIR . 'includes/**/*.php');
+ $allFiles = array_merge($rootFiles, $subdirectoryFiles);
+
+ foreach($allFiles as $filename){
+   include_once($filename); // avoid duplicate errors
+ }
+
+ //print_r($subdirectoryFiles); //print has to be used when showing arrays, can be done with echo
+
+//  include(UP_PLUGIN_DIR . 'includes/register-blocks.php');
+//  include(UP_PLUGIN_DIR . 'includes/blocks/search-form.php');
+//  include(UP_PLUGIN_DIR . 'includes/blocks/page-header.php');
+
 
  // Hooks
  add_action( 'init', 'up_register_blocks' );
+
+
